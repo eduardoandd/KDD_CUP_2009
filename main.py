bbc01ba34b80dc4b24b29410be4bf490c318d392
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-08-28 14:39:50 -0300

    importações,carregamento e divisao entre vars numericas e categoricas

--- main.py
+++ main.py
@@ -6,3 +6,23 @@
 #	Tendência de comprar novos produtos ou serviços
 #	Pretensão de comprar upgrades ou adicionais
 
+# ======================= IMPORTAÇÕES =======================
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import ExtraTreesClassifier,RandomForestClassifier,GradientBoostingClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split,cross_val_predict,GridSearchCV
+
+# ======================= CARREGAMENTO =======================
+features=pd.read_csv('orange_small_test.data',sep='\t', na_filter=False) #atributos previsores
+#1=sim -1=nao
+outcome= pd.read_csv('orange_small_train_churn.labels', header=None) # variavel dependente
+np.unique(outcome)
+
+# ======================= VAR NUMERICAS E CATEGORICAS =======================
+all_var=np.array(features.columns)
+num_vars=np.array(all_var[0:190])
+cat_vars=np.array(all_var[190:])
+
+
