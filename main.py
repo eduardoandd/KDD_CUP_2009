f232301f134a6cbd1f67ed6064c3405fbbfcbc70
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-08-28 13:38:15 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# ======================= OBJETIVO =======================
+# A ideia geral envolve a criação de um modelo para predição dessas 3 variáveis com base 
+# nos dados fornecidos por essa base de dados real de uma empresa de telecomunicação francesa.
+
+#	Possibilidade do cliente fazer cancelamento da conta
+#	Tendência de comprar novos produtos ou serviços
+#	Pretensão de comprar upgrades ou adicionais
+
